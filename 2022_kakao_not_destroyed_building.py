d157f8c26a361f0f88b1163279c1906825d7adfb
JMsuper <tjsqls2067@naver.com>
2022-09-07 12:44:35 +0900

    분할 정복까지는 생각해 냈지만, 가로-세로 모두 나눠서 분할 정복을 하려던 탓에 너무 복잡해졌다.

--- 2022_kakao_not_destroyed_building.py
+++ 2022_kakao_not_destroyed_building.py
@@ -1,22 +1,42 @@
-# 스킬을 사용하는 순서는 중요하지 않다.
-
-# 스킬들을 조합하여 한번에 board에 적용하자
-# 어떻게 스킬들을 조합?
-
 def solution(board, skill):
     answer = 0
     
+    # 이 반복문들을 한번에 처리할 수 있는 방법이 없을까?
+    # 보드에 접근하여 하나씩 수정해나가는 것은 비효율적
+
+    # 동일한 영역을 다루는 스킬들에 대해 스킬을 분리하여
+    # 통합하거 상쇄시키면 되지 않을까?
+    clustered_skill = {}
     for t, r1, c1, r2, c2, degree in skill:
-        if t == 1:
-            for i in range(r1,r2+1):
-                for j in range(c1,c2+1):
-                    board[i][j] -= degree
+        if (r1,c1) in clustered_skill:
+            clustered_skill[(r1,c1)].append([t,r1,c1,r2,c2,degree])
         else:
-            for i in range(r1,r2+1):
-                for j in range(c1,c2+1):
-                    board[i][j] += degree
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] > 0:
-                answer += 1
+            clustered_skill[(r1,c1)] = [[t,r1,c1,r2,c2,degree]]
+    print(clustered_skill)
+
+    # # 이거 분할정복이다
+    # for t, r1, c1, r2, c2, degree in skill:
+    #     if t == 1:
+    #         for i in range(r1,r2+1):
+    #             for j in range(c1,c2+1):
+    #                 board[i][j] -= degree
+    #     else:
+    #         for i in range(r1,r2+1):
+    #             for j in range(c1,c2+1):
+    #                 board[i][j] += degree
+    
+    
+    
+    # for i in range(len(board)):
+    #     for j in range(len(board[0])):
+    #         if board[i][j] > 0:
+    #             answer += 1
     return answer
+
+def main():
+    board = [[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]]
+    skill = [[1,0,0,3,4,4],[1,2,0,2,3,2],[2,1,0,3,1,2],[1,0,1,3,3,1]]
+    solution(board,skill)
+
+if __name__ == "__main__":
+    main()
