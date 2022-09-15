bcf077481d5b738e29f0747ccc8787981a48688e
JMsuper <tjsqls2067@naver.com>
2022-09-15 20:50:38 +0900

    플로이드 워셜에 재귀함수 추가. but! 실패

--- 2021_kakao_card_pair_select.py
+++ 2021_kakao_card_pair_select.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 INF = int(1e9)
 
 def floyd_warshall(graph):
@@ -32,46 +35,71 @@ def create_graph(board):
                     graph[b_len * i + j][b_len * cx + cy] = 1
     return graph
 
+def recursive(card_dict,board,cnt,cx,cy):
+    b_len = len(board)
+    graph = create_graph(board)
+    distance = floyd_warshall(graph)
+    target_subject_dict = {}
+    for key, value in card_dict.items():
+        for x, y in value:
+            target_subject_dict[(x,y)] = distance[cx * b_len + cy][x * b_len + y]
+    for key, value in target_subject_dict.items():
+        for item in card_dict[board[key[0]][key[1]]]:
+            if item[0] != key[0] or item[1] != key[1]:
+                target_subject_dict[key] = value + distance[key[0] * b_len + key[1]][item[0] * b_len + item[1]] 
+    subject_list = []
+    t_distance = INF
+    for key, value in target_subject_dict.items():
+        if value < t_distance:
+            subject_list = [[key[0],key[1]]]
+            t_distance = value
+            continue
+        elif value == t_distance:
+            subject_list.append([key[0],key[1]])
+    
+    min_ret = INF
+    for item in subject_list:
+        cx, cy = item[0], item[1]
+        card_value = board[cx][cy]
+        copy_board = deepcopy(board)
+        copy_card_dict = deepcopy(card_dict)
+        for x,y in card_dict[card_value]:
+            copy_board[x][y] = 0
+        del copy_card_dict[card_value]
+        if copy_card_dict:
+            ret = recursive(copy_card_dict,copy_board,cnt + t_distance + 2, cx, cy)
+            if ret < min_ret:
+                min_ret = ret
+    if min_ret == INF:
+        return cnt + t_distance + 2
+    else:
+        return min_ret
+
+
             
 def solution(board, r, c):
     answer = 0
-
-    graph = create_graph(board)
-    distance = floyd_warshall(graph)
-    print(distance)
-
+    card_dict = {}
+    b_len = len(board)
+    for i in range(b_len):
+        for j in range(b_len):
+            if board[i][j] != 0:
+                if board[i][j] in card_dict:
+                    card_dict[board[i][j]].append([i,j])
+                else:
+                    card_dict[board[i][j]] = [[i,j]]
+    cx, cy = r, c
+    answer = recursive(card_dict,board,0,cx,cy)
     return answer
 
 
 
 def main():
-    graph = [
-        [1,1,0],
-        [0,3,3],
-        [2,0,2]
-    ]
-    r = 1
+    graph = [[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]]	
+    r = 0
     c = 1
-    solution(graph,r,c)
-    # board = [
-    #     [1,1,0],
-    #     [0,3,3],
-    #     [2,0,2]
-    # ]
+    print(solution(graph,r,c))
 
-    # graph_t = create_graph(board)
-
-    # graph = [
-    #     [ 0 , 1 ,INF, 1 ,INF,INF, 1 ,INF,INF],
-    #     [ 1 , 0 , 1 ,INF, 1 ,INF,INF,INF,INF],
-    #     [INF, 1 , 0 ,INF,INF, 1 ,INF,INF,INF],
-    #     [ 1 ,INF,INF, 0 , 1 ,INF, 1 ,INF,INF],
-    #     [INF, 1 ,INF, 1 , 0 , 1 ,INF, 1 ,INF],
-    #     [INF, 1 ,INF,INF, 1 , 0 ,INF,INF, 1 ],
-    #     [ 1 ,INF,INF, 1 ,INF,INF, 0 , 1 , 1 ],
-    #     [INF,INF,INF,INF, 1 ,INF, 1 , 0 , 1 ],
-    #     [INF,INF,INF,INF,INF, 1 , 1 , 1 , 0 ],
-    # ]
 
 if __name__ == "__main__":
     main()
