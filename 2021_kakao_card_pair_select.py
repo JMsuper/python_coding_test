a1489fe695343c18269b32ec10533c6767ca294f
JMsuper <tjsqls2067@naver.com>
2022-09-16 20:19:20 +0900

    순열, BFS 사용

--- 2021_kakao_card_pair_select.py
+++ 2021_kakao_card_pair_select.py
@@ -1,85 +1,74 @@
-from copy import deepcopy
+from itertools import permutations
+from collections import deque
 
+# 모든 경우의 수 찾기 -> ok
+# 각 경우 실행
+# BFS를 통한 최단 경로 찾기
 
-INF = int(1e9)
+# 각 경우의 수를 실행할 떄 마다 최솟값 갱신
+# 만약 특정 경우를 실행시키는 데 현재 최솟값 보다 클 경우 해당 경우 종료
 
-def floyd_warshall(graph):
-    graph_len = len(graph)
-    distance = graph[:]
-    for i in range(graph_len):
-        for j in range(graph_len):
-            for k in range(graph_len):
-                distance[j][k] = min(distance[j][k],distance[j][i] + distance[i][k])
-    return distance
+INF = 10000000
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
+def recursive(card_dict,subject_list,subject, cur_subject,i):
+    # 정해진 순서 배열
+    # 첫번째 or 두번째
+    # 몇번째꺼 추가?
+    if len(subject) * 2  == len(cur_subject):
+        subject_list.append(cur_subject)
+        return
+    for j in range(2):
+        if j == 0:
+            recursive(card_dict,subject_list,subject,cur_subject + [card_dict[subject[i]][0],card_dict[subject[i]][1]],i+1)
+        else:
+            recursive(card_dict,subject_list,subject,cur_subject + [card_dict[subject[i]][1],card_dict[subject[i]][0]],i+1)
 
-def create_graph(board):
+def shortest_path_bfs(board,cx,cy,tx,ty):
+    queue = deque([])
     b_len = len(board)
-    g_len = b_len * b_len
-    graph = [[INF for _ in range(g_len)] for _ in range(g_len)]
-    for i in range(g_len):
-        graph[i][i] = 0
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-    for i in range(b_len):
-        for j in range(b_len):
-            for k in range(4):
-                cx, cy = i + dx[k], j + dy[k]
-                if 0 <= cx < b_len and 0 <= cy < b_len:
-                    graph[b_len * i + j][b_len * cx + cy] = 1
-                    while board[cx][cy] == 0:
-                        cx, cy = cx + dx[k], cy + dy[k]
-                        if 0 > cx or cx >= b_len or 0 > cy or cy >= b_len:
-                            cx, cy = cx - dx[k], cy - dy[k]
-                            break
-                    graph[b_len * i + j][b_len * cx + cy] = 1
-    return graph
+    visited = [[False for _ in range(b_len)] for _ in range(b_len)]
 
-def recursive(card_dict,board,cnt,cx,cy):
-    b_len = len(board)
-    graph = create_graph(board)
-    distance = floyd_warshall(graph)
-    target_subject_dict = {}
-    for key, value in card_dict.items():
-        for x, y in value:
-            target_subject_dict[(x,y)] = distance[cx * b_len + cy][x * b_len + y]
-    for key, value in target_subject_dict.items():
-        for item in card_dict[board[key[0]][key[1]]]:
-            if item[0] != key[0] or item[1] != key[1]:
-                target_subject_dict[key] = value + distance[key[0] * b_len + key[1]][item[0] * b_len + item[1]] 
-    subject_list = []
-    t_distance = INF
-    for key, value in target_subject_dict.items():
-        if value < t_distance:
-            subject_list = [[key[0],key[1]]]
-            t_distance = value
-            continue
-        elif value == t_distance:
-            subject_list.append([key[0],key[1]])
-    
-    min_ret = INF
-    for item in subject_list:
-        cx, cy = item[0], item[1]
-        card_value = board[cx][cy]
-        copy_board = deepcopy(board)
-        copy_card_dict = deepcopy(card_dict)
-        for x,y in card_dict[card_value]:
-            copy_board[x][y] = 0
-        del copy_card_dict[card_value]
-        if copy_card_dict:
-            ret = recursive(copy_card_dict,copy_board,cnt + t_distance + 2, cx, cy)
-            if ret < min_ret:
-                min_ret = ret
-    if min_ret == INF:
-        return cnt + t_distance + 2
-    else:
-        return min_ret
+    queue.append([cx,cy,0])
+    while True:
+        cx, cy, depth = queue.popleft()
+        visited[cx][cy] = True
+        if cx == tx and cy == ty:
+            return depth
+        for i in range(4):
+            kx, ky = cx + dx[i], cy + dy[i]
+            if 0 <= kx < b_len and 0 <= ky < b_len and not visited[kx][ky]:
+                queue.append([kx,ky,depth + 1])
+                should_append = False
+                while board[kx][ky] == 0:
+                    kx, ky = kx + dx[i], ky + dy[i]
+                    if 0 > kx or kx >= b_len or 0 > ky or ky >= b_len:
+                        kx, ky = kx - dx[i], ky - dy[i]
+                        break
+                    else:
+                        should_append = True
+                if should_append:
+                    queue.append([kx,ky,depth + 1])
 
+def research(board,path,r,c,min_cnt):
+    ret = 0
+    cx, cy = r, c
+    for i in range(len(path)):
+        if ret >= min_cnt:
+            return INF
+        tx, ty = path[i]
+        ret += shortest_path_bfs(board,cx,cy,tx,ty) + 1
+        cx, cy = tx, ty
+        if i % 2 == 1:
+            board[path[i][0]][path[i][1]] = 0
+            board[path[i-1][0]][path[i-1][1]] = 0
+    return ret
 
-            
 def solution(board, r, c):
-    answer = 0
     card_dict = {}
+    card_value_list = []
+    subject_list = []
     b_len = len(board)
     for i in range(b_len):
         for j in range(b_len):
@@ -88,16 +77,23 @@ def solution(board, r, c):
                     card_dict[board[i][j]].append([i,j])
                 else:
                     card_dict[board[i][j]] = [[i,j]]
-    cx, cy = r, c
-    answer = recursive(card_dict,board,0,cx,cy)
-    return answer
+                    card_value_list.append(board[i][j])
+    
+    for item in permutations(card_value_list,len(card_value_list)):
+        recursive(card_dict,subject_list,list(item),[],0)
 
+    min_cnt = INF
+    for path in subject_list:
+        temp_board = [item[:] for item in board]
+        cnt = research(temp_board,path,r,c,min_cnt)
+        min_cnt = min(min_cnt,cnt)
 
+    return min_cnt
 
 def main():
-    graph = [[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]]	
-    r = 0
-    c = 1
+    graph = [[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]]
+    r = 1
+    c = 0
     print(solution(graph,r,c))
 
 
